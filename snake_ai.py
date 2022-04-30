d951a52bdc77f4ff1114b4e4c54aa7b106d5697d
Joseph <fjoe.joe1212@gmail.com>
2022-04-30 14:57:08 -0400

    Refactored watching the trained model play

--- snake_ai.py
+++ snake_ai.py
@@ -1,26 +1,24 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecFrameStack
+import os
+import time
+from envs import SnakeEnv, SnakeEnv1D
 
-from snake_env import SnakeEnv, SnakeEnv1D
-
-models_dir = f'models/1646516997'
-model_path = f'{models_dir}/120000.zip'
+model_path = os.path.join("models", "full", "1650815114", "model_5000000.zip")
 
 # env = SnakeEnv()
-env = make_vec_env(SnakeEnv, n_envs=1)
-env = VecFrameStack(env, n_stack=4, channels_order='last')
-env.reset()
+env = make_vec_env(SnakeEnv, n_envs=1, env_kwargs={"grid_x": 10, "grid_y": 10})
+env = VecFrameStack(env, n_stack=4, channels_order="last")
 
 model = PPO.load(model_path, env=env)
 
-while True:
+for _ in range(20):
     obs = env.reset()
     done = False
-
     while not done:
-        env.render()
         action, _states = model.predict(obs)
         obs, rewards, done, info = env.step(action)
-        # print(rewards)
+        env.render()
+        # time.sleep(0.01)
     env.close()
